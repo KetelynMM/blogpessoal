601b22a3eb8be129bde1ae43af59967fb1cd6832
Ketelyn Medina <ketelynmedina5@gmail.com>
2023-04-17 09:53:19 -0300

    Blog Pessoal

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,11 +1,15 @@
 package com.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 
-@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
-
+	//@Param(por causa da consulta tipo LIKE)  T  e ID os roxos , titulo é mudavel  
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo); 
+	// é um metodo de consula ^ ^ 
+	// SELECT * FROM tb_postagens WHERE titulo LIKE "%????%"; sql
 }
